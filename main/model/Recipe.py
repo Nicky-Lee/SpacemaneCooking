be6a3b418590030e96701693417f6b2f4906255a
Nicky-Lee <871511711@qq.com>
2022-06-29 00:35:29 +0800

    变更

--- main/model/Recipe.py
+++ main/model/Recipe.py
@@ -1,10 +1,10 @@
 from .. import db
 
 
-# recipe_ingredient = db.Table('recipe_ingredient',
-#                              db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
-#                              db.Column('igd_id', db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
-#                              )
+recipe_ingredient = db.Table('recipe_ingredient',
+                             db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
+                             db.Column('igd_id', db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
+                             )
 
 
 # ingredient_tag = db.Table('ingredient_tag',
@@ -18,6 +18,17 @@ from .. import db
 #     db.Column("Recipe_id", db.Integer, db.ForeignKey("Recipe.id")))
 # )
 
+"""
+R_category=
+{
+
+1:breakfast
+2:lunch
+3:dinner
+4:dessert
+}
+"""
+
 
 class Recipe(db.Model):
     __tablename__ = 'recipe'
@@ -25,11 +36,12 @@ class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     R_name = db.Column(db.String(80), nullable=True)
     R_description = db.Column(db.String(240), nullable=True)
-    R_category = db.Column(db.String(120), nullable=True)
+    R_category = db.Column(db.String(20), nullable=True)
     R_calorie = db.Column(db.Integer, nullable=True)
     image_id = db.Column(db.Integer)
     # igd_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    Ingredient = db.relationship('Ingredient', secondary=recipe_ingredient)
 
     def __init__(self, R_name, user_id, R_description=None, R_category=None, R_calorie=0, R_img_url=None):
         self.R_name = R_name
@@ -43,53 +55,6 @@ class Recipe(db.Model):
         return '<Recipe %r>' % self.R_name
 
 
-# class IGD_category(db.Model):
-#     __tablename__ = 'igd_category'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     igd_category = db.Column(db.String(30), unique=True, nullable=False)
-#     igd_id = db.Column(db.Integer,db.ForeignKey('ingredient.id'))
-#
-#     # igd=db.relationship('Ingredient',secondary=ingredient_tag)
-#
-#     # igd_id = db.relationship("Ingredient", secondary=ingredient_tag, backref="IGD_category")
-#     # recipe = db.relationship("recipe", backref="ingredient")
-#     # recipe = db.Column(db.Integer, db.ForeignKey('recipe.id'))
-#     # recipe = db.relationship('recipe', secondary=recipe_ingredient,backref='ingredient')
-#
-#     def __init__(self, igd_category):
-#         self.igd_category = igd_category
-
-
-# class Ingredient(db.Model):
-#     __tablename__ = 'ingredient'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     igd_name = db.Column(db.String(80), unique=True, nullable=False)
-#     igd_category = db.Column(db.String(120), nullable=True)
-#     igd_opponent = db.Column(db.String(80))
-#     igd_calorie = db.Column(db.Integer, nullable=True)
-#     image_id = db.Column(db.Integer)
-#     # recipe = db.Column(db.Integer,db.ForeignKey('Recipe.id'))
-#
-#
-#     # recipe = db.relationship("Recipe", secondary=association_table, backref="Ingredient")
-#     # recipe = db.relationship("recipe", backref="ingredient")
-#     # recipe = db.Column(db.Integer, db.ForeignKey('recipe.id'))
-#     # recipe = db.relationship('recipe', secondary=recipe_ingredient,backref='ingredient')
-#
-#     def __init__(self, igd_name, igd_category=None,igd_opponent=None, igb_description=None, igd_calorie=0, image_id=None):
-#         self.igd_name = igd_name
-#         self.igd_category=igd_category
-#         self.igd_opponent = igd_opponent
-#         self.igb_description = igb_description
-#         self.igd_calorie = igd_calorie
-#         self.image_id =image_id
-#
-#     def __repr__(self):
-#         return '<ingredient %r>' % self.igd_name
-
-#
 tags = db.Table('tags'
                 , db.Column('igd_category_id', db.Integer, db.ForeignKey('igd_category.id'))
                 , db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id')))
@@ -115,7 +80,7 @@ class Ingredient(db.Model):
     igd_opponent = db.Column(db.String(80))
     igd_calorie = db.Column(db.Integer, nullable=True)
     image_id = db.Column(db.Integer)
-    # Recipe = db.relationship('Recipe', secondary=Recipe_table)
+    Recipe = db.relationship('Recipe', secondary=recipe_ingredient)
 
     # student_id=db.Column(db.Integer,db.ForeignKey('student.id'))
     def __init__(self, igd_name, igd_category=None, igd_opponent=None, igb_description=None, igd_calorie=0,
--- main/server/search.py
+++ main/server/search.py
@@ -5,19 +5,40 @@ from .. import *
 from sqlalchemy import and_, or_
 import json
 from ..util.token import TOKEN
-
+from collections import defaultdict
 
 
 def process_search_igd(request):
     igd_info = json.loads(request.data)
     igd_name = "%{}%".format(igd_info['igd_name'])
     igd = Ingredient.query.filter(Ingredient.igd_name.like(igd_name)).all()
+
     return igd
 
 
 def process_search_igd_list(request):
     igd_info = json.loads(request.data)
     igd_name_list = igd_info['igd_name'].split(',')
+
     igd_list = Ingredient.query.filter(Ingredient.igd_name.in_(igd_name_list)).all()
     return igd_list
 
+def process_Search_recipe(request):
+    igd_info = json.loads(request.data)
+    igd_name_list = igd_info['igd_name'].split(',')
+    R_category = igd_info['R_category']
+    igd_list = Ingredient.query.filter(Ingredient.igd_name.in_(igd_name_list)).all()
+    R_id_list = defaultdict(int)
+    for igd in igd_list:
+        R_list = igd.Recipe
+        for R in R_list:
+            if R.R_category == R_category:
+                R_id_list[R]+=1
+
+    print(R_id_list)
+    # R = Recipe.query.join(test.Recipe)
+    # print(R)
+    # R_list = Recipe.query.filter(and_(Recipe.R_category == R_category,Recipe.Ingredient.igd_name.in_(igd_name_list))).all()
+    # return R_list
+
+    return 0
